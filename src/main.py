2fe29a43f8f1d7c6f7d506c2ea50dbc0a161bd01
probablynotval <162844653+probablynotval@users.noreply.github.com>
2024-06-15 19:14:28 +0000

    Add maze class

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from map import Cell
+from map import Maze
 from window_builder import Line
 from window_builder import Point
 from window_builder import Window
@@ -6,11 +7,13 @@ from window_builder import Window
 
 def main():
     window = Window(800, 800)
-    window.draw_line(Line(Point(200, 200), Point(800, 800)), "blue")
+    # window.draw_line(Line(Point(200, 200), Point(800, 800)), "blue")
 
-    cell = Cell(window)
-    cell.has_bottom_wall = False
-    cell.draw_walls(Point(350, 350), Point(475, 475))
+    # cell = Cell(window)
+    # cell.has_bottom_wall = False
+    # cell.draw_walls(Point(350, 350), Point(475, 475))
+
+    Maze(window, Point(0, 0), Point(50, 50), 5, 5)
 
     window.wait_for_close()
 
--- src/map.py
+++ src/map.py
@@ -1,3 +1,5 @@
+import time
+
 from draw import Line
 from draw import Point
 from window_builder import Window
@@ -43,3 +45,35 @@ class Cell:
         center_x = self._x1 + ((self._x2 - self._x1) / 2)
         center_y = self._y1 + ((self._y2 - self._y1) / 2)
         return Point(center_x, center_y)
+
+
+class Maze:
+    def __init__(self, window: Window, pos: Point, size: Point, rows: int, cols: int):
+        self._window = window
+        self._pos = pos
+        self._size = size
+        self._rows = rows
+        self._cols = cols
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = [
+            [Cell(self._window) for _ in range(self._cols)] for _ in range(self._rows)
+        ]
+        for i in range(self._rows):
+            for j in range(self._cols):
+                self._draw_cell(Point(i, j))
+
+    def _draw_cell(self, index: Point):
+        x_pos = self._pos._x + index._x * self._size._x
+        y_pos = self._pos._y + index._y * self._size._y
+        self._cells[index._x][index._y]._pos = Point(x_pos, y_pos)
+        cell: Cell = self._cells[index._x][index._y]
+        cell.draw_walls(
+            Point(x_pos, y_pos), Point(x_pos + self._size._x, y_pos + self._size._y)
+        )
+        self._animate()
+
+    def _animate(self):
+        self._window.redraw()
+        time.sleep(0.05)
